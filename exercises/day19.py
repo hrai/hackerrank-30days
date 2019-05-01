f0c75d0cb19cb1e76d9d7e5096c4fd789eeb4288
Hangjit Rai <rai.hangjit@gmail.com>
2019-05-01 19:51:22 +1000

    Added Day19

---
+++ exercises/day19.py
@@ -0,0 +1,23 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        sum = 0
+
+        for num in range(1, n + 1):
+            if n % num == 0:
+                sum += num
+
+        return sum
+
+        pass
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
